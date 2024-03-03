b4835bd16a97009627e4529d68ea5804160f5a49
devamdoshi212 <devamdoshi212@gmail.com>
2024-03-03 14:33:33 +0530

    update

--- main.js
+++ main.js
@@ -1,85 +1,61 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
-const path = require("node:path");
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require("electron");
+const MainScreen = require("./screens/main/mainScreen");
+const { autoUpdater, AppUpdater } = require("electron-updater");
+
+let curWindow;
+
+//Basic flags
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
 
 function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-
-  // and load the index.html of the app.
-  // mainWindow.loadFile("index.html");
-  mainWindow.loadURL("https://quizportalfrontend.onrender.com/");
-  mainWindow.on("minimize", () => {
-    console.log("Window is minimized");
-    // Handle window minimize event
-  });
-
-  mainWindow.on("restore", () => {
-    console.log("Window is restored");
-    // Handle window restore event
-  });
-  mainWindow.on("resize", () => {
-    const size = mainWindow.getSize();
-    console.log(size);
-    console.log(`Window resized to ${size[0]}x${size[1]}`);
-    // Handle window resize event
-  });
-  mainWindow.on("blur", () => {
-    console.log("Window lost focus");
-    // Handle window blur event
-  });
-
-  mainWindow.on("focus", () => {
-    console.log("Window focused");
-    // Handle window focus event
-  });
+  curWindow = new MainScreen();
 }
-// Open the DevTools.
-// mainWindow.webContents.openDevTools()
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length == 0) createWindow();
   });
 
-  const mainMenu = Menu.buildFromTemplate([
-    {
-      label: "Application",
-      submenu: [
-        {
-          label: "Quit",
-          accelerator: process.platform === "darwin" ? "Cmd+Q" : "Ctrl+Q",
-          click() {
-            app.quit();
-          },
-        },
-      ],
-    },
-  ]);
-
-  // Set the application menu
-  Menu.setApplicationMenu(mainMenu);
+  autoUpdater.checkForUpdates();
+  curWindow.showMessage(
+    `Checking for updates. Current version ${app.getVersion()}`
+  );
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+/*New Update Available*/
+autoUpdater.on("update-available", (info) => {
+  curWindow.showMessage(
+    `Update available. Current version ${app.getVersion()}`
+  );
+  let pth = autoUpdater.downloadUpdate();
+  curWindow.showMessage(pth);
+});
+
+autoUpdater.on("update-not-available", (info) => {
+  curWindow.showMessage(
+    `No update available. Current version ${app.getVersion()}`
+  );
+});
+
+/*Download Completion Message*/
+autoUpdater.on("update-downloaded", (info) => {
+  curWindow.showMessage(
+    `Update downloaded. Current version ${app.getVersion()}`
+  );
+});
+
+autoUpdater.on("error", (info) => {
+  curWindow.showMessage(info);
+});
+
+//Global exception handler
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
+
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform != "darwin") app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
